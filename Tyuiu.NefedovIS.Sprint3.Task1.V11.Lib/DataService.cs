104353d14715476bee59562118330fef0991ebe0
Иван Нефедов <Tyuiu.NefedovIS@outlook.com>
2024-10-19 15:47:29 +0500

    Task1

--- Tyuiu.NefedovIS.Sprint3.Task1.V11.Lib/DataService.cs
+++ Tyuiu.NefedovIS.Sprint3.Task1.V11.Lib/DataService.cs
@@ -6,11 +6,12 @@ namespace Tyuiu.NefedovIS.Sprint3.Task1.V11.Lib
     {
         public double GetMultiplySeries(double value, int startValue, int stopValue)
         {
-            double res = 1;
+            double res = 1; 
             while (startValue <= stopValue)
-                {
-                    res *= Math.Pow(value, startValue) + 1.0 / 2;
-                }
+            {
+                res *= Math.Pow(value, startValue) + 1.0 / 2; 
+                startValue++;
+            }
             return Math.Round(res, 3);
         }
     }
