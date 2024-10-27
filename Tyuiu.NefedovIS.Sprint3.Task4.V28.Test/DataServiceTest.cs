37ad176335f8f220ea3365fb9b4c0c6651d6ec93
NefedovIvanS <Tyuiu.NefedovIS@outlook.com>
2024-10-27 17:37:15 +0500

    Task4

---
+++ Tyuiu.NefedovIS.Sprint3.Task4.V28.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.NefedovIS.Sprint3.Task4.V28.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.NefedovIS.Sprint3.Task4.V28/Program.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.NefedovIS.Sprint3.Task4.V28.Lib;
+
+namespace Tyuiu.NefedovIS.Sprint3.Task4.V28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            int x = -5;
+            int y = 5;
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate(x, y));
+            Console.ReadKey();
+        }
+    }
+}
