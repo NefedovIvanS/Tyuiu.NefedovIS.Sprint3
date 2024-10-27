c8ff32bda78a8b66205a7bc5e9099aad583966e2
NefedovIvanS <Tyuiu.NefedovIS@outlook.com>
2024-10-27 16:14:32 +0500

    Task3

--- Tyuiu.NefedovIS.Sprint3.Task3.V8.Lib/DataService.cs
+++ Tyuiu.NefedovIS.Sprint3.Task3.V8.Lib/DataService.cs
@@ -1,6 +1,8 @@
-﻿namespace Tyuiu.NefedovIS.Sprint3.Task3.V8.Lib
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.NefedovIS.Sprint3.Task3.V8.Lib
 {
-    public class DataService
+    public class DataService: ISprint3Task3V8
     {
         public int ConvertStringToInt(string value)
         {
