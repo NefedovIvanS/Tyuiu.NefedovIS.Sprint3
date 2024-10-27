afa86eef959597eb3865d7c995bf439d36090db0
NefedovIvanS <Tyuiu.NefedovIS@outlook.com>
2024-10-28 01:45:48 +0500

    Task5

---
+++ Tyuiu.NefedovIS.Sprint3.Task5.V12.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.NefedovIS.Sprint3.Task5.V12.Lib
+{
+    public class DataService : ISprint3Task5V12
+    {
+        public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
+        {
+            double res = 0;
+            for (int i = startValue1; i <= stopValue1; i++)
+            {
+                for (int j = startValue2; j <= stopValue2; j++)
+                {
+                    res += Math.Cos(j) + x / 2.0;
+                }
+            }
+            return Math.Round(res, 3);
+        }
+    }
+}
---
+++ Tyuiu.NefedovIS.Sprint3.Task5.V12/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.NefedovIS.Sprint3.Task5.V12.Lib;
+
+namespace Tyuiu.NefedovIS.Sprint3.Task5.V12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            int x = 5;
+            int start1 = 1;
+            int end1 = 3;
+            int start2 = 1;
+            int end2 = 10;
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(x);
+            Console.WriteLine(start1);
+            Console.WriteLine(end1);
+            Console.WriteLine(start2);
+            Console.WriteLine(end2);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.GetSumSumSeries(x, start1, start2, end1, end2));
+            Console.ReadKey();
+        }
+    }
+}
