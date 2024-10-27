dad5b25f60c16079ebcd05d679150b7b33b4d0b5
NefedovIvanS <Tyuiu.NefedovIS@outlook.com>
2024-10-27 16:15:28 +0500

    Task3

--- Tyuiu.NefedovIS.Sprint3.Task3.V8.Lib/DataService.cs
+++ Tyuiu.NefedovIS.Sprint3.Task3.V8.Lib/DataService.cs
@@ -4,7 +4,7 @@ namespace Tyuiu.NefedovIS.Sprint3.Task3.V8.Lib
 {
     public class DataService: ISprint3Task3V8
     {
-        public int ConvertStringToInt(string value)
+        public int ConvertStringToInt(string value) //
         {
             string str = "";
             foreach (char c in value)
